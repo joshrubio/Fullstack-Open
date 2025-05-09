71eea97c8e2c7fd27244334161cbadae98bb5080
joshrubio <155823600+joshrubio@users.noreply.github.com>
2025-05-10 01:18:17 +0200

    add complete exercises 1.4 and 1.5

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,32 +1,53 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course=
-        {course}
-      />
-      <Content course=
-      
-         {part1} {exercises1}
-      
-        {part2} {exercises2}
-      
-        {part3} {exercises3}
-      />
-      <Total course=
-        {exercises1 + exercises2 + exercises3}
-      />
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>
+}
 
+const Content = ({ parts })  => {
+  return (
+    <div>
+      {parts.map(part => (
+        <p key={part.name}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </div>
+  )
+}
 
+const Total = ({parts}) => {
+  const total = parts.reduce ((sum, part) => sum + part.exercises, 0)
+  return (
+    <p>
+      Number of exercises {total}
+    </p>
+  )
+}
 export default App
